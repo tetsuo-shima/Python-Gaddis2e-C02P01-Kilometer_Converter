21263c4ef1f66684d272a77ab74ac4275a480d97
Dwight Francis <dfrancis0@gmail.com>
2014-09-11 15:59:04 -0400

    first draft

--- Kilometer_Converter.py
+++ Kilometer_Converter.py
@@ -1,3 +1,17 @@
 __author__ = 'dwight'
 
 # Write a program that asks the user to enter a distance in kilometers, and then converts that distance to miles.
+
+
+def main():
+    print('Kilometers to Miles Converter')
+    print('-----------------------------')
+    kilometers = float(input('Enter number of kilometers: '))
+    print(kilometers, 'km =', km_to_mi(kilometers), 'mi')
+
+
+def km_to_mi(kilometers):
+    return kilometers * 0.6214
+
+
+main()
